4e79c106f13f2842965eb1a0ccb0846c0bfd0995
Anthony Cruz <cruz@Anthonys-MBP.fios-router.home>
2019-01-05 20:16:30 -0500

    added external dependencies for styling

--- new-win-site/src/app/app.module.ts
+++ new-win-site/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './containers/home/home.component';
+import { AboutComponent } from './containers/about/about.component';
+import { MenuComponent } from './containers/menu/menu.component';
+import { ContactUsComponent } from './containers/contact-us/contact-us.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuComponent,
+    ContactUsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
